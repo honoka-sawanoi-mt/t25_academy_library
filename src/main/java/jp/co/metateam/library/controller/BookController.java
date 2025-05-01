d39951d8bc626101b7d62abcb2726eefe19ccb50
honoka-sawanoi-mt <honoka.sawanoi@metateam.co.jp>
2025-05-01 16:14:21 +0900

    Revert "書籍一覧画面の説明文追加"

--- src/main/java/jp/co/metateam/library/controller/BookController.java
+++ src/main/java/jp/co/metateam/library/controller/BookController.java
@@ -20,8 +20,6 @@ import jp.co.metateam.library.model.BookMstDto;
 import jp.co.metateam.library.service.BookMstService;
 import lombok.extern.log4j.Log4j2;
 
-//フロントにデータを渡すところ
-
 /**
  * 書籍関連クラス
  */
